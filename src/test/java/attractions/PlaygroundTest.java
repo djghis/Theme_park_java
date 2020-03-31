3c53876239eb4b1b8541926ac77e4ffd554a2af1
djghis <djghis@hotmail.com>
2020-03-31 19:56:34 +0100

    initial commit

---
+++ src/test/java/attractions/PlaygroundTest.java
@@ -0,0 +1,45 @@
+package attractions;
+
+import org.junit.Before;
+import org.junit.Test;
+import people.Visitor;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlaygroundTest {
+    Playground playground;
+    Visitor visitor1;
+    Visitor visitor2;
+
+    @Before
+    public void setUp() throws Exception {
+        playground = new Playground("Fun Zone", 7);
+        visitor1 = new Visitor(12,1.10, 100);
+        visitor2 = new Visitor(18,1.80, 500);
+
+
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Fun Zone", playground.getName());
+    }
+
+    @Test
+    public void hasRating() {
+        assertEquals(7, playground.getRating());
+    }
+
+    @Test
+    public void hasVisitCount() {
+        assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void checkAge(){
+        assertEquals(true, playground.isAllowedTo(visitor1));
+        assertEquals(false, playground.isAllowedTo(visitor2));
+
+
+    }
+}
